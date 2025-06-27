f4a9d829413ab096419330062c32e905e503276e
Hector Flores <hflores@rn.grandvelas>
2025-06-27 16:08:47 -0600

    Class for Model Admin | question & pub_date

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -21,7 +21,6 @@ class IndexView(generic.ListView):
         ]
 
 class DetailView(generic.DetailView):
-    model = Question
     template_name = "polls/detail.html"
 
      def get_queryset(self):
