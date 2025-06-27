0f1055228fa7fdbfee313bfe4d0ff65a807a8330
Hector Flores <hflores@rn.grandvelas>
2025-06-27 16:23:57 -0600

    register() method | Django t07

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
+
+class ChoiceInLine(admin.StackedInLine):
+    model = Choice
+    extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ["pub_date", "question_text"]
+    # fields = ["pub_date", "question_text"]
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInLine]
 
 admin.site.register(Question, QuestionAdmin)
+
+# admin.site.register(Question, QuestionAdmin)
