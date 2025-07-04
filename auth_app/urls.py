6782edac0f76f3675aa045c73663fb4e8eea1b96
Hector Flores <hflores@rn.grandvelas>
2025-07-04 16:51:14 -0600

    defualt view

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'auth_app'
 
 urlpatterns = [
+    path('', views.login_view, name='login'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('forgot-password/', views.forgot_password_view, name='forgot_password'),
